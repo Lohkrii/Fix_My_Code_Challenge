645525a86d2a2eafdef58011a5e93ab98a3fbcc1
Joshua Lopez <joshualopez0429@gmail.com>
2021-05-18 18:18:53 +0000

    Added comments and documentation on functions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
+""" Instantiates a square class object """
 class square():
-    
+    """ Is the class in question """
     width = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initializes the class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +14,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Calculates the perimeter of the square """
         return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
+        """ Returns the size of the square """
         return "{}/{}".format(self.width, self.width)
 
 if __name__ == "__main__":
